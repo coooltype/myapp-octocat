885da64e97fb33e89f79c4d16432e423b90ef422
yoonhwan <yoonhwan.work@gmail.com>
2023-12-01 15:18:01 +0900

    [Modify] index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     index.html을 위한 js 파일
 */
 
-function add(a,b, c+10) {
+function add(a,b, c = 10) {
     if (C !== undefined) {
         return a+b+c;
     }
@@ -11,4 +11,12 @@ function add(a,b, c+10) {
 
 function multiply(a, b) {
     return a*b;
+}
+
+function substract (a, b) {
+    if (a > b) {
+        return a-b;
+    } else {
+        return b-a;
+    }
 }
